4f36d043a1103fea44826bd28011608ae5a28a83
sysim <liv660n@gmail.com>
2023-08-18 08:29:51 +0900

    lint 사용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,26 @@
-// TODO: eslint를 구성해 보세요. 
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ import MainController from './controllers/MainController.js'
 
 import './app.scss'
 
-const foo = ''
-
 document.addEventListener("DOMContentLoaded", ()=>{
-new MainController();;;; 
+new MainController(); 
 })
